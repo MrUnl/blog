6d70f8055f96794fd232c7dafd361ab90c974540
MrUnl <aslanataberk06@gmail.com>
2020-11-12 23:20:19 +0300

    Under Development Page Added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,26 @@ function App() {
     
     return (
         <>
-        <a href="http://instagram.com/mr_unl">Ataberk ASLAN</a>'s Personal Website
+        <img src="consturction.svg"></img>
+        <div className="texts">
+            <p>This Site is Under Development</p>
+            <div className="social-text">
+                <div></div>
+                <h3>Socials</h3>
+                <div></div>
+            </div>
+            <div className="socials">
+            <a className="instagram" target="_blank" href="https://instagram.com/mr_unl">
+                <span class="fa fa-instagram"></span>
+            </a>
+            <a className="github" target="_blank" href="https://github.com/MrUnl">
+                <span class="fa fa-github"></span>
+            </a>
+            <a className="twitter" target="_blank" href="https://twitter.com/mr_unl">
+                <span class="fa fa-twitter"></span>
+            </a>
+            </div>
+        </div>
         </>
     );
 }
